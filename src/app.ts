eb57073fa61f456ea1fbbecdebb366cbb19b2459
dzefa <danchong625@gmail.com>
2019-03-20 20:18:56 -0700

    [feat] adds middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import { corsOptions } from './utils/cors';
 
 const app = express();
 
 // Set App Properties
 app.set(`PORT`, process.env.PORT);
+
+// Middlewares
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('short'));
 
 export { app };
