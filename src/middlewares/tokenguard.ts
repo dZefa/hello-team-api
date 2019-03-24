8b642eb2268d7f8695d8172a1817902658f68f10
dzefa <danchong625@gmail.com>
2019-03-23 18:04:09 -0700

    [guard] initializes token guard

---
+++ src/middlewares/tokenguard.ts
@@ -0,0 +1,35 @@
+import { IncomingHttpHeaders } from 'http';
+import { RequestHandler, NextFunction, Response, Request } from 'express';
+
+import { UserService } from '../services/user.service';
+
+const userService = new UserService();
+
+const getToken = (headers: IncomingHttpHeaders): string => {
+  const authHeader = headers.authorization;
+
+  return authHeader;
+};
+
+export const tokenGuard: (() => RequestHandler) = (() => async (req: Request, res: Response, next: NextFunction) => {
+  const token = getToken(req.headers);
+
+  if (!token) {
+    return res.status(412).send({ result: { error: `MISSING_HEADERS` } });
+  }
+
+  try {
+    const userId = await userService.verifyToken(token);
+
+    if (typeof userId === 'number') {
+      res.locals.userId = userId;
+    }
+  }
+  catch (err) {
+    console.log(`Error in token guard. ERROR: ${err}`);
+
+    return res.status(500).send({ result: { error: err } });
+  }
+
+  next();
+});
