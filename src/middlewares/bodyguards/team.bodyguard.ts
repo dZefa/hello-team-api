81930fb14b95e9296651100717b61add258bd6cc
dzefa <danchong625@gmail.com>
2019-03-23 20:22:58 -0700

    [guard] initializes team body guard

---
+++ src/middlewares/bodyguards/team.bodyguard.ts
@@ -0,0 +1,26 @@
+import { RequestHandler, NextFunction, Request, Response } from 'express';
+
+import { TeamAddModel } from '../../db/models/team.model';
+
+export const teamBodyGuard: ((type: string) =>  RequestHandler) = ((type) => (req: Request, res: Response, next: NextFunction) => {
+  let body: TeamAddModel | any;
+  let isValid: boolean;
+
+  switch (type) {
+    case 'team-register':
+      body = req.body as TeamAddModel;
+      isValid = !!body.userId && !!body.name && !!body.type;
+
+      break;
+    
+    default:
+      console.log(`Error in Team bodyguard. Hit DEFAULT`);
+      return res.status(500).send({ result: { error: `SERVER_ERROR` } });
+  }
+
+  if (!isValid) {
+    return res.status(400).send({ result: { error: `MISSING_INFORMATION` } });
+  }
+
+  next();
+});
