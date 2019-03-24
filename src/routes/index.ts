dd86aad25448dcd406bcb626d7b0a1603514cedf
dzefa <danchong625@gmail.com>
2019-03-23 18:14:33 -0700

    [route] adds tokenguard and team route to route entry

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import { tokenGuard } from '../middlewares/tokenguard';
+
 import { userRouter } from './user.route';
+import { teamRouter } from './team.route';
 
 const router = Router();
 
 router.use('/user', userRouter);
+router.use('/team', tokenGuard(), teamRouter);
 
 export { router };
