f76b7fa81a9008ecb87556c1dbbff9717b6874a6
dzefa <danchong625@gmail.com>
2019-03-23 20:33:36 -0700

    [serv] implements entry service file to initialize all services

---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+import { TeamService } from './team.service';
+import { UserService } from './user.service';
+
+export const userService = new UserService();
+export const teamService = new TeamService();
